35db511897708c0796408b6cc466286798e958ac
kevingermainbusiness <kevingermainbusiness@gmail.com>
2020-12-01 19:58:52 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Kevin Germain on 01/12/2020.
+//  Copyright © 2020 Kevin Germain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
