22d27fff45f87edcc0987d51c4a57e03202e2737
kevingermainbusiness <kevingermainbusiness@gmail.com>
2020-12-03 20:04:18 -0500

    A lot of UI changes that I kinda understand thankfully, Glory be to God!!

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Kevin Germain on 03/12/2020.
+//  Copyright © 2020 Kevin Germain. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
